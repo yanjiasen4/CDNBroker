3ca795d6fa81458def8c8637b6e201d90a1cb15a
yanjiasen4 <13122169550@163.com>
2017-05-02 17:06:09 +0800

    server ip detect

--- client.py
+++ client.py
@@ -4,6 +4,13 @@ import time
 import io
 import shutil
 import urllib
+import socket
+import platform
+import wmi
+
+
+brokerServerIP = 'localhost'
+brokerServerPort = 9800
 
 
 class MonitorServer(BaseHTTPRequestHandler):
@@ -33,7 +40,15 @@ class ServerThread(threading.Thread):
         self.monitorServer = monitorServer
 
     def run(self):
+        self.connectInit()
         self.monitorServer.serve_forever()
+
+    def connectInit(self):
+        #clientIP = socket.gethostbyname(socket.gethostname())
+        clientIP = '59.78.22.135'  # test
+        skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        skt.sendto(bytes(clientIP, encoding='utf-8'),
+                   (brokerServerIP, brokerServerPort))
 
 
 class MonitorThread(threading.Thread):
@@ -50,58 +65,71 @@ class MonitorThread(threading.Thread):
             threadLock.acquire()
             global transmitBytesPerSec
             netstat = self.get_net_stat()
-            curr_transmitBytes = float(netstat[1]['TransmitBytes'])/1024/8
+            curr_transmitBytes = sum(float(iface['TransmitBytes']) for iface in netstat) / 1024 / 8
             diff_transmitBytes = curr_transmitBytes - self.last_transmitBytes
-            transmitBytesPerSec = diff_transmitBytes/self.interval
+            transmitBytesPerSec = diff_transmitBytes / self.interval
             self.last_transmitBytes = curr_transmitBytes
             print(transmitBytesPerSec)
             threadLock.release()
 
     def get_net_stat(self):
         net = []
-        f = open("/proc/net/dev")
-        lines = f.readlines()
-        f.close()
-        for line in lines[2:]:
-            con = line.split()
-            """ 
-            intf = {} 
-            intf['interface'] = con[0].lstrip(":") 
-            intf['ReceiveBytes'] = int(con[1]) 
-            intf['ReceivePackets'] = int(con[2]) 
-            intf['ReceiveErrs'] = int(con[3]) 
-            intf['ReceiveDrop'] = int(con[4]) 
-            intf['ReceiveFifo'] = int(con[5]) 
-            intf['ReceiveFrames'] = int(con[6]) 
-            intf['ReceiveCompressed'] = int(con[7]) 
-            intf['ReceiveMulticast'] = int(con[8]) 
-            intf['TransmitBytes'] = int(con[9]) 
-            intf['TransmitPackets'] = int(con[10]) 
-            intf['TransmitErrs'] = int(con[11]) 
-            intf['TransmitDrop'] = int(con[12]) 
-            intf['TransmitFifo'] = int(con[13]) 
-            intf['TransmitFrames'] = int(con[14]) 
-            intf['TransmitCompressed'] = int(con[15]) 
-            intf['TransmitMulticast'] = int(con[16]) 
-            """
-            intf = dict(
-                zip(
-                    ('interface', 'ReceiveBytes', 'ReceivePackets',
-                     'ReceiveErrs', 'ReceiveDrop', 'ReceiveFifo',
-                     'ReceiveFrames', 'ReceiveCompressed', 'ReceiveMulticast',
-                     'TransmitBytes', 'TransmitPackets', 'TransmitErrs',
-                     'TransmitDrop', 'TransmitFifo', 'TransmitFrames',
-                     'TransmitCompressed', 'TransmitMulticast'),
-                    (con[0].rstrip(":"), int(con[1]), int(con[2]),
-                     int(con[3]), int(con[4]), int(con[5]),
-                     int(con[6]), int(con[7]), int(con[8]),
-                     int(con[9]), int(con[10]), int(con[11]),
-                     int(con[12]), int(con[13]), int(con[14]),
-                     int(con[15]), int(con[16]), )
-                )
-            )
+        os = platform.system()
 
-            net.append(intf)
+        if os == 'Windows':
+            c = wmi.WMI()
+            for iface in c.Win32_PerfRawData_Tcpip_TCPv4():
+                sentflow = float(iface.SegmentsSentPersec)  # 已发送的流量
+                intf = {}
+                intf['interface'] = iface.name
+                intf['TransmitBytes'] = sentflow
+
+                net.append(intf)
+
+        elif os == 'Linux':
+            f = open("/proc/net/dev")
+            lines = f.readlines()
+            f.close()
+            for line in lines[2:]:
+                con = line.split()
+                """ 
+                intf = {} 
+                intf['interface'] = con[0].lstrip(":") 
+                intf['ReceiveBytes'] = int(con[1]) 
+                intf['ReceivePackets'] = int(con[2]) 
+                intf['ReceiveErrs'] = int(con[3]) 
+                intf['ReceiveDrop'] = int(con[4]) 
+                intf['ReceiveFifo'] = int(con[5]) 
+                intf['ReceiveFrames'] = int(con[6]) 
+                intf['ReceiveCompressed'] = int(con[7]) 
+                intf['ReceiveMulticast'] = int(con[8]) 
+                intf['TransmitBytes'] = int(con[9]) 
+                intf['TransmitPackets'] = int(con[10]) 
+                intf['TransmitErrs'] = int(con[11]) 
+                intf['TransmitDrop'] = int(con[12]) 
+                intf['TransmitFifo'] = int(con[13]) 
+                intf['TransmitFrames'] = int(con[14]) 
+                intf['TransmitCompressed'] = int(con[15]) 
+                intf['TransmitMulticast'] = int(con[16]) 
+                """
+                intf = dict(
+                    zip(
+                        ('interface', 'ReceiveBytes', 'ReceivePackets',
+                        'ReceiveErrs', 'ReceiveDrop', 'ReceiveFifo',
+                        'ReceiveFrames', 'ReceiveCompressed', 'ReceiveMulticast',
+                        'TransmitBytes', 'TransmitPackets', 'TransmitErrs',
+                        'TransmitDrop', 'TransmitFifo', 'TransmitFrames',
+                        'TransmitCompressed', 'TransmitMulticast'),
+                        (con[0].rstrip(":"), int(con[1]), int(con[2]),
+                        int(con[3]), int(con[4]), int(con[5]),
+                        int(con[6]), int(con[7]), int(con[8]),
+                        int(con[9]), int(con[10]), int(con[11]),
+                        int(con[12]), int(con[13]), int(con[14]),
+                        int(con[15]), int(con[16]), )
+                    )
+                )
+
+                net.append(intf)
 
         return net
 
--- server.py
+++ server.py
@@ -1,12 +1,107 @@
 #-*- coding: UTF-8 -*-
-import socket,select
+import socket
+import select
 import sys
-import _thread
+import threading
 import requests
-
-if __name__=='__main__':      
-    proxies = { "http": "http://23.83.247.144:3128" }
-    r = requests.get("http://cdnbroker.tech",  proxies=proxies) 
-    print(r.elapsed)
+import json
 
 
+ipParserUrl = 'http://api.ip138.com/query/?datatype=jsonp&token=c7fbe6a2583ed6c76847560e89960e82&ip='
+
+nodePool = {}
+
+class CDNNode(object):
+    def __init__(self, ip, addr, maxt):
+        self.ip = ip
+        self.addr = addr
+        self.maxTransmit = maxt
+        self.currTransmit = 0
+
+    def __cmp__(self, other):
+        for i in range(min(len(self.addr, other,addr))):
+            if self.addr[i] > other.addr[i]:
+                return 1
+            elif self.addr[i] == other.addr[i]:
+                continue
+            else:
+                return -1
+        return 0
+    
+    def __str__(self):
+        return str(self.ip) 
+
+    '''
+    The larger return value is, the more closer 2 nodes are.
+    lower bound is 0
+    '''
+    def cmpRegion(self, other, maxlevel=None):
+        value = 0
+        cmpLevel = min(len(self.addr), len(other.addr))
+        if maxlevel is not None:
+            cmpLevel = min(cmpLevel, maxlevel)
+        for i in range(cmpLevel):
+            if self.addr[i] == other.addr[i]:
+                value += 1
+            else:
+                break
+        return value
+    
+    '''
+    whetehr 2 nodes are in a same region.
+    maxlevel denote the compare level for address.
+    '''
+    def inSameRegion(self, other, maxlevel=None):
+        addrLevel = len(self.addr)
+        if addrLevel != len(other.addr):
+            return Fasle
+        if maxlevel is not None:
+            addrLevel = maxlevel
+        value = self.cmpRegion(other, maxlevel=addrLevel)
+        return value == addrLevel
+
+    def isOverloaded(self):
+        return self.currTransmit >= self.maxTransmit
+
+class ClientListener(threading.Thread):
+    def __init__(self, threadID, name, port):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.port = port
+
+    def run(self):
+        self.connectClient(self.port)
+
+    def connectClient(self, port):
+        skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        skt.bind(('', port))
+        while True:
+            data, addr = skt.recvfrom(1024)
+            r = requests.get(ipParserUrl+str(data, encoding='utf-8'))
+            print('Recived:', str(data, encoding='utf-8'),
+                'from', addr)
+            data = eval(r.text)
+            print(data)
+            addr = tuple(data['data'][:2])
+            node = CDNNode(data['ip'], data['data'], 1024)
+            addNode2Pool(node, addr)
+            print(str(node) + '添加成功，所属区域' + str(addr))
+
+def access(url, proxies):
+    r = requests.get(url, proxies=proxies)
+
+
+def addNode2Pool(node, addr):
+    if not addr in nodePool:
+        nodePool[addr] = []
+    nodePool[addr].append(node)
+
+if __name__ == '__main__':
+    threads = []
+
+    listener = ClientListener(0, 'listener', 9800)
+    
+    listener.start()
+
+    threads.append(listener)
